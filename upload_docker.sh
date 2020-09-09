f8d0e5a853cdbd6cfd7ec1a1bc17e6ce76864d6a
Lakshmi Bhargavi Rangavajjula <43711076+rlbth@users.noreply.github.com>
2020-09-09 18:22:25 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
-
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="clairelee7522/mlapi"
+
+dockerpath="rlbth/udacity-op-ml:lastest"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=rlbth
 echo "Docker ID and Image: $dockerpath"
-docker login &&\
-    docker image tag mlapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker image push $dockerpath
+docker tag udacity-op-ml:lastest $dockerpath
+docker push $dockerpath
