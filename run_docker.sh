5c173d94617d8ebecb347c96fadefada15e41f16
Lakshmi Bhargavi Rangavajjula <43711076+rlbth@users.noreply.github.com>
2020-09-09 18:15:48 +0530

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=mlapi .
+docker build -t udacity-op-ml:latest
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 mlapi
+docker run -p 8000:80 udacity-op-ml:latest
