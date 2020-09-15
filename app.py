55386f5b4a6d2bd76749199bdaf87fccd512f225
Lakshmi Bhargavi Rangavajjula <43711076+rlbth@users.noreply.github.com>
2020-09-15 15:19:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-import joblib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
     
-    LOG.info("Scaling Payload: \n{payload}")
+    LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
@@ -55,15 +55,15 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
-    LOG.info("JSON payload: \n{json_payload}")
+    LOG.info("JSON payload: \n"+json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("Inference payload DataFrame: \n{inference_payload}")
+    LOG.info("Inference payload DataFrame: \n"+inference_payload)
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info("Output prediction: {prediction}")
+    LOG.info("Prediction returned by the model is :" +prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
