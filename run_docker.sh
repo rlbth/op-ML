dba5e2f75b3f5233172747a2207ca4256ae1305c
Lakshmi Bhargavi Rangavajjula <43711076+rlbth@users.noreply.github.com>
2020-09-17 23:49:48 +0530

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t docker.io/lakran21/lakran:latest .
+docker build -t lakran21/lakran:latest .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images ls
 
 # Step 3: 
 # Run flask app
-docker run -d -p 8000:80 docker.io/lakran21/lakran:latest
+docker run -d -p 8000:80 lakran21/lakran:latest
