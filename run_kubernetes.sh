3e86baffe2bfcf188d8be7d95917a8650df4b2a0
Lakshmi Bhargavi Rangavajjula <43711076+rlbth@users.noreply.github.com>
2020-09-20 22:46:11 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,11 +5,11 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="docker.io/lakran21/lakran"
+dockerpath="lakran21/lakran"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run lakran21/lakran --image=$dockerpath --port=80 --labels app=lakran21/lakran
+kubectl run lakran21/lakran --image=$dockerpath --port=80 --labels app=lakran
 
 
 # Step 3:
@@ -18,5 +18,5 @@ kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment lakran21/lakran --type="NodePort" --port=80
-kubectl port-forward lakran21/lakran 8000:80
+kubectl expose deployment lakran --type="NodePort" --port=80
+kubectl port-forward lakran 8000:80
